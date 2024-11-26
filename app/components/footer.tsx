5d89e3cb81a53294f9c98443f4deb2730b712349
Jesse Wei <jesseweijapan@gmail.com>
2024-11-26 21:12:28 +0900

    add social media links to footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
   return (
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
-        <li>
+        {/* <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
@@ -28,6 +28,17 @@ export default function Footer() {
           >
             <ArrowIcon />
             <p className="ml-2 h-7">rss</p>
+          </a>
+        </li> */}
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.linkedin.com/in/jesse-wei-profile/"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">LinkedIn</p>
           </a>
         </li>
         <li>
@@ -38,7 +49,7 @@ export default function Footer() {
             href="https://github.com/mrjwei"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">github</p>
+            <p className="ml-2 h-7">GitHub</p>
           </a>
         </li>
         <li>
@@ -46,10 +57,10 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="https://www.instagram.com/mrjwei/"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
+            <p className="ml-2 h-7">Instagram</p>
           </a>
         </li>
       </ul>
