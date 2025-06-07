c7c859f052e3cd9a8b3f4ad40d6dff0424164de8
Jesse Wei <jesseweijapan@gmail.com>
2025-06-07 16:38:30 +1000

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,16 @@
-import { getWritings } from 'app/utils'
+import { getAllSortedWritings } from "app/utils"
 
-export const baseUrl = 'https://www.jessewei.net'
+export const baseUrl = "https://www.jessewei.net"
 
 export default async function sitemap() {
-  let writings = getWritings().map((post) => ({
-    url: `${baseUrl}/writings/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
+  let writings = getAllSortedWritings().map((writing) => ({
+    url: `${baseUrl}/writings/${writing.slug}`,
+    lastModified: writing.metadata.publishedAt,
   }))
 
-  let routes = ['', '/writings'].map((route) => ({
+  let routes = ["", "/writings"].map((route) => ({
     url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
+    lastModified: new Date().toISOString().split("T")[0],
   }))
 
   return [...routes, ...writings]
