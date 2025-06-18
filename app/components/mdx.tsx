af6e452a5c16115285fffab1b3c1d1af0340589c
Jesse Wei <jesseweijapan@gmail.com>
2025-06-18 21:16:49 +1000

    add new post

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -1,24 +1,28 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import { MDXRemote } from 'next-mdx-remote/rsc'
-import { highlight } from 'sugar-high'
-import React from 'react'
+import Link from "next/link"
+import Image from "next/image"
+import { MDXRemote } from "next-mdx-remote/rsc"
+import { highlight } from "sugar-high"
+import React from "react"
 
 function Table({ data }) {
   let headers = data.headers.map((header, index) => (
-    <th key={index}>{header}</th>
+    <th key={index} className="pr-8 py-2">
+      {header}
+    </th>
   ))
   let rows = data.rows.map((row, index) => (
-    <tr key={index}>
+    <tr key={index} className="border-b-1 border-neutral-300">
       {row.map((cell, cellIndex) => (
-        <td key={cellIndex}>{cell}</td>
+        <td key={cellIndex} className="py-2">
+          {cell}
+        </td>
       ))}
     </tr>
   ))
 
   return (
     <table>
-      <thead>
+      <thead className="w-screen border-b-1 border-neutral-300">
         <tr>{headers}</tr>
       </thead>
       <tbody>{rows}</tbody>
@@ -29,7 +33,7 @@ function Table({ data }) {
 function CustomLink(props) {
   let href = props.href
 
-  if (href.startsWith('/')) {
+  if (href.startsWith("/")) {
     return (
       <Link href={href} {...props}>
         {props.children}
@@ -37,7 +41,7 @@ function CustomLink(props) {
     )
   }
 
-  if (href.startsWith('#')) {
+  if (href.startsWith("#")) {
     return <a {...props} />
   }
 
@@ -58,10 +62,10 @@ function slugify(str) {
     .toString()
     .toLowerCase()
     .trim() // Remove whitespace from both ends of a string
-    .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(/&/g, '-and-') // Replace & with 'and'
-    .replace(/[^\w\-]+/g, '') // Remove all non-word characters except for -
-    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(/&/g, "-and-") // Replace & with 'and'
+    .replace(/[^\w\-]+/g, "") // Remove all non-word characters except for -
+    .replace(/\-\-+/g, "-") // Replace multiple - with single -
 }
 
 function createHeading(level) {
@@ -71,10 +75,10 @@ function createHeading(level) {
       `h${level}`,
       { id: slug },
       [
-        React.createElement('a', {
+        React.createElement("a", {
           href: `#${slug}`,
           key: `link-${slug}`,
-          className: 'anchor',
+          className: "anchor",
         }),
       ],
       children
