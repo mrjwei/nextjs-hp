e2d3aaa1b2901278b1be4e380a5b97fd4a326608
Jesse Wei <jesseweijapan@gmail.com>
2025-06-21 17:47:22 +1000

    improvement: adjust tag styles

--- app/components/tag.tsx
+++ app/components/tag.tsx
@@ -1,6 +1,9 @@
-export function Tag({label, color}) {
+export function Tag({ label, color }) {
   return (
-    <div className="text-xs font-medium px-2 py-0.5 rounded-full flex justify-center" style={{border: `2px solid ${color}`, color}}>
+    <div
+      className="text-xs font-bold px-2 py-0.5 rounded-full flex justify-center"
+      style={{ border: `2px solid ${color}`, color }}
+    >
       {label}
     </div>
   )
