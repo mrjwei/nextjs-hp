956996e11bf74e4e9b734c799df721f2952ebb1d
Jesse Wei <jesseweijapan@gmail.com>
2026-03-26 07:30:50 +1000

    fix: update font configurations for Spartan and Techno, ensuring proper weight and display settings

--- app/data/fonts.ts
+++ app/data/fonts.ts
@@ -4,16 +4,19 @@ export const openSans = Open_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
   style: ["normal", "italic"],
+  display: "swap",
 })
 
 export const spartan = League_Spartan({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
+  weight: ["400", "700"],
   style: ["normal"],
+  display: "swap",
 })
 
 export const techno = Roboto_Slab({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800", "900"],
+  weight: ["900"],
   style: ["normal"],
+  display: "swap",
 })
