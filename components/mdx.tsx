b857c1fe99b9f8171a3c7f2265e37759eb3e1c47
Jesse Wei <jesseweijapan@gmail.com>
2026-08-29 15:51:43 +1000

    fix: update slugify function to allow Unicode characters in slugs

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -73,7 +73,7 @@ function slugify(str) {
     .trim() // Remove whitespace from both ends of a string
     .replace(/\s+/g, "-") // Replace spaces with -
     .replace(/&/g, "-and-") // Replace & with 'and'
-    .replace(/[^\w\-]+/g, "") // Remove all non-word characters except for -
+    .replace(/[^\p{L}\p{N}_\-]+/gu, "") // Remove all non-word characters except for -
     .replace(/\-\-+/g, "-") // Replace multiple - with single -
 }
 
